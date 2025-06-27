bc0fefc82aa2cda304f07dcd5ef45a636cbc08cc
Yaminhaqani <yaminhaqani@gmail.com>
2025-06-28 02:32:27 +0530

    added profilepic

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className='fixed top-0 left-0  h-[8vh] right-0 z-50 bg-black'>
+    <header className='fixed top-0 left-0  h-[9vh] right-0 z-50 bg-black'>
       <div className='max-w-7xl mx-auto px-5 sm:px-10'>
         <div className='flex justify-between items-center py-5 mx-auto'>
           {/* Site Logo */}
@@ -42,8 +42,11 @@ const Navbar = () => {
             whileHover={{ scale: 1.1 }}
             transition={{ type: 'spring', stiffness: 200, damping: 10 }}
             href="/"
-            className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'
+            className='text-neutral-400 font-bold text-xl hover:text-white transition-colors flex flex-row items-center gap-2'
           >
+            <div className='w-8 h-8 rounded-full bg-white overflow-hidden'>
+              <img src="/assets/me.jpg" alt="me" className='w-full h-full object-cover object-[center_40%]'/>
+          </div>
             Yamin
           </motion.a>
 
